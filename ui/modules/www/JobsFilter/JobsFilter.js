0d7844dfc62307281aecd1986e970e24fdc07792
Mike Stop Continues <mikestopcontinues@gmail.com>
2016-06-16 13:53:39 -0400

    filter improvements

--- ui/modules/www/JobsFilter/JobsFilter.js
+++ ui/modules/www/JobsFilter/JobsFilter.js
@@ -4,18 +4,26 @@ import React, {Component, PropTypes} from 'react';
 import styles from './JobsFilter.css';
 import cn from 'classnames';
 import _ from 'lodash';
+import {getJobNiceInterval} from '../JobsHelper/JobsHelper.js';
+import {connect} from 'react-redux';
 
 // vars
 
-const searchFields = ['code', 'description', 'interval', 'name', 'resultQuery'];
+const searchFields = ['code', 'description', 'interval', 'name', 'resultQuery', 'statusEmail', 'resultEmail'];
 
 // export
 
+@connect((state) => {
+  return {
+    useLocalTime: state.localStorage.useLocalTime === 'true',
+  };
+})
 export default class JobsFilter extends Component {
   static propTypes = {
     className: PropTypes.string,
     jobs: PropTypes.array,
     onFilter: PropTypes.func.isRequired,
+    useLocalTime: PropTypes.bool.isRequired,
   };
 
   state = {
@@ -43,14 +51,23 @@ export default class JobsFilter extends Component {
   }
 
   filterJobs() {
+    const {useLocalTime} = this.props;
     const textFilter = this.state.text;
 
     this.props.onFilter((this.props.jobs || []).filter((job) => {
       if (textFilter.length) {
         return textFilter.every((text) => {
+          if (_.includes(getJobNiceInterval(job, useLocalTime).toLowerCase(), text)) {
+            return true;
+          }
+
           return _.some(job, (val, key) => {
             if (!_.isString(val) || searchFields.indexOf(key) === -1) {
               return false;
+            }
+
+            if (Array.isArray(val)) {
+              val = val.join(', ');
             }
 
             return _.includes(val.toLowerCase(), text);
